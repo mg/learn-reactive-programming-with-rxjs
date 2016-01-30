7ce367ec64035923c0ff6aca9e832a15b0ab4cb3
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-30 23:00:23 +0000

    ch3: use requestAnimationFrame scheduler for starfield animaiton

--- src/chapter3/starfield.js
+++ src/chapter3/starfield.js
@@ -3,6 +3,16 @@ import Rx from 'rx'
 const SPEED= 40
 const STAR_COUNT= 250
 
+function animationLoop() {
+  return Rx.Observable.generate(
+    0,          // start value
+    () => true, // loop forever
+    x => x + 1, // increment
+    x => x,     // value on each notification
+    Rx.Scheduler.requestAnimationFrame
+  )
+}
+
 // Starfields stream factory
 export default function(canvas) {
   const star= () => {
@@ -13,10 +23,11 @@ export default function(canvas) {
     }
   }
 
-  return Rx.Observable.range(1, STAR_COUNT)
+  return Rx.Observable
+    .range(1, STAR_COUNT)
     .map(star)
     .toArray()
-    .flatMap(starsArray => Rx.Observable.interval(SPEED).map(() => {
+    .flatMap(starsArray => animationLoop().map(() => { // replaces Rx.Observable.interval(SPEED).map(...)
       starsArray.forEach(star => {
         star.y= star.y > canvas.height ? 0 : star.y + 3
       })
